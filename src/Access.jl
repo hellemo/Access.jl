ff2c3dc2833d0efb8e4d23907c9e55a5f2775a54
Lars Hellemo <hellemo@gmail.com>
2020-10-25 08:57:34 +0100

    Formatting fixes (Blue style)

--- src/Access.jl
+++ src/Access.jl
@@ -4,41 +4,48 @@ using JavaCall
 using JDBC, DataFrames
 
 function __init__()
-
-    libs = ["commons-lang3-3.8.1.jar","commons-logging-1.2.jar","hsqldb-2.5.0.jar","jackcess-3.0.1.jar"]
+    libs = [
+        "commons-lang3-3.8.1.jar",
+        "commons-logging-1.2.jar",
+        "hsqldb-2.5.0.jar",
+        "jackcess-3.0.1.jar",
+    ]
     for l in libs
-        fn = joinpath(@__DIR__,"..","UCanAccess","lib",l)
+        fn = joinpath(@__DIR__, "..", "UCanAccess", "lib", l)
         JavaCall.addClassPath(fn)
     end
-    
-    fn = joinpath(@__DIR__,"..","UCanAccess","ucanaccess-5.0.0.jar")
-    fn = replace(fn,"\\" => "/")
-    
-    JDBC.usedriver(joinpath(@__DIR__,"..","UCanAccess","ucanaccess-5.0.0.jar"))
+
+    fn = joinpath(@__DIR__, "..", "UCanAccess", "ucanaccess-5.0.0.jar")
+    fn = replace(fn, "\\" => "/")
+
+    JDBC.usedriver(joinpath(@__DIR__, "..", "UCanAccess", "ucanaccess-5.0.0.jar"))
     JDBC.init()
-    
+
     return nothing
 end
 
-function filecursor(fn;immediaterelease=false,noinactivitytimeout=false)
-    fn = replace(fn,"\\" => "/")    
-    timeout =""
+function filecursor(fn; immediaterelease=false, noinactivitytimeout=false)
+    fn = replace(fn, "\\" => "/")
+    timeout = ""
     if noinactivitytimeout
         timeout = ";inactivityTimeout=0"
     end
-    
-    cursor("jdbc:ucanaccess://$fn;immediatelyReleaseResources=$(string(immediaterelease))$(timeout)")
+
+    return cursor("jdbc:ucanaccess://$fn;immediatelyReleaseResources=$(string(immediaterelease))$(timeout)")
 end
 
-
-function query(fn,qstr;imrelease=false,notimeout=true)
-    fn = replace(fn,"\\" => "/")    
-    df = JDBC.load(DataFrame,filecursor(fn;immediaterelease=imrelease,noinactivitytimeout=notimeout), qstr)
+function query(fn, qstr; imrelease=false, notimeout=true)
+    fn = replace(fn, "\\" => "/")
+    df = JDBC.load(
+        DataFrame,
+        filecursor(fn; immediaterelease=imrelease, noinactivitytimeout=notimeout),
+        qstr,
+    )
     return df
 end
 
 # TODO Quote strings
-function createInsert(df,tablename)
+function createInsert(df, tablename)
     function qstring(s)
         return s
     end
@@ -48,40 +55,38 @@ function createInsert(df,tablename)
     function qkey(s)
         return "[" * string(s) * "]"
     end
-    
+
     ns = names(df)
     SQLquery = String[]
     for r in eachrow(df)
-        keys = join((qkey(n) for n in ns),", ")
-        vals = join((qstring(r[n]) for n in ns),", ")
-        push!(SQLquery,"INSERT INTO $tablename ($keys) VALUES ($vals);")
+        keys = join((qkey(n) for n in ns), ", ")
+        vals = join((qstring(r[n]) for n in ns), ", ")
+        push!(SQLquery, "INSERT INTO $tablename ($keys) VALUES ($vals);")
     end
     return SQLquery
 end
 
-function query!(fn,SQLQuery;imrelease=false,notimeout=false)
-    csr = filecursor(fn;immediaterelease=imrelease,noinactivitytimeout=notimeout)
-    execute!(csr,SQLQuery)
+function query!(fn, SQLQuery; imrelease=false, notimeout=false)
+    csr = filecursor(fn; immediaterelease=imrelease, noinactivitytimeout=notimeout)
+    return execute!(csr, SQLQuery)
 end
 
 # Insert all rows from a DataFrame into table"
-function insertdf(fn,df::DataFrame,tablename::String;imrelease=false,notimeout=true)
-    
-    csr = filecursor(fn;immediaterelease=imrelease,noinactivitytimeout=notimeout)
+function insertdf(fn, df::DataFrame, tablename::String; imrelease=false, notimeout=true)
+    csr = filecursor(fn; immediaterelease=imrelease, noinactivitytimeout=notimeout)
     ns = names(df)
-    SQLquery = createInsert(df,tablename)
+    SQLquery = createInsert(df, tablename)
     for q in SQLquery
-        execute!(csr,q)
+        execute!(csr, q)
     end
 end
 
 # Insert all rows from a DataFram into table with same name as DataFrame
-macro insertdf(fn,df)
+macro insertdf(fn, df)
     return quote
         tn = $(string(df))
-        insertdf($(esc(fn)),$(esc(df)),tn)
+        insertdf($(esc(fn)), $(esc(df)), tn)
     end
 end
-
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,48 +1,49 @@
 using Test
 using Access
 
-fn = cp(joinpath(@__DIR__,"TestDatabase.accdb"),joinpath(mktempdir(;cleanup=false),"Test.accdb"))
+fn = cp(
+    joinpath(@__DIR__, "TestDatabase.accdb"),
+    joinpath(mktempdir(; cleanup=false), "Test.accdb"),
+)
 
 @testset "Read data from db" begin
-   
-# fn = joinpath(@__DIR__,"TestDatabase.accdb")
-@test isfile(fn)
-df = Access.query(fn,"SELECT * FROM Person WHERE BirthYear > 1980")   
 
-@test size(df,1) == 1 # One Row
-@test df.FirstName[1] == "Slim"
-@test df.LastName[1] == "Shady"
-@test df.ID[1] == 3
+    # fn = joinpath(@__DIR__,"TestDatabase.accdb")
+    @test isfile(fn)
+    df = Access.query(fn, "SELECT * FROM Person WHERE BirthYear > 1980")
+
+    @test size(df, 1) == 1 # One Row
+    @test df.FirstName[1] == "Slim"
+    @test df.LastName[1] == "Shady"
+    @test df.ID[1] == 3
 end
-
-
 
 @testset "Modify db data" begin
 
-# Read table content to DataFrame
-StoredValues = Access.query(fn,"SELECT * FROM StoredValues;");
+    # Read table content to DataFrame
+    StoredValues = Access.query(fn, "SELECT * FROM StoredValues;")
 
-# Delete everything in table
-Access.query!(fn,"DELETE * FROM StoredValues;")
+    # Delete everything in table
+    Access.query!(fn, "DELETE * FROM StoredValues;")
 
-# Check table empty?
-shouldbeemtpy = Access.query(fn,"SELECT * FROM StoredValues;");
-@test size(shouldbeemtpy,1) == 0
+    # Check table empty?
+    shouldbeemtpy = Access.query(fn, "SELECT * FROM StoredValues;")
+    @test size(shouldbeemtpy, 1) == 0
 
-# Re-insert data from DataFrame
-Access.@insertdf(fn,StoredValues)
-shouldnotbeemtpy = Access.query(fn,"SELECT * FROM StoredValues;");
-@test size(shouldnotbeemtpy,1) > 0
+    # Re-insert data from DataFrame
+    Access.@insertdf(fn, StoredValues)
+    shouldnotbeemtpy = Access.query(fn, "SELECT * FROM StoredValues;")
+    @test size(shouldnotbeemtpy, 1) > 0
 
-# Check table content
-@test shouldnotbeemtpy[6,4] == "Mashall Mathers"
-@test shouldnotbeemtpy[2,3] ≈ 3.3
+    # Check table content
+    @test shouldnotbeemtpy[6, 4] == "Mashall Mathers"
+    @test shouldnotbeemtpy[2, 3] ≈ 3.3
 
-# Alternatively, specify table explicitly:
-Access.query!(fn,"DELETE * FROM StoredValues;")
-Access.insertdf(fn,StoredValues,"StoredValues")
-shouldnotbeemtpy = Access.query(fn,"SELECT * FROM StoredValues;");
-@test shouldnotbeemtpy[2,3] ≈ 3.3
+    # Alternatively, specify table explicitly:
+    Access.query!(fn, "DELETE * FROM StoredValues;")
+    Access.insertdf(fn, StoredValues, "StoredValues")
+    shouldnotbeemtpy = Access.query(fn, "SELECT * FROM StoredValues;")
+    @test shouldnotbeemtpy[2, 3] ≈ 3.3
 end
 
 # TODO: Figure out why this (and cleanup) fails
